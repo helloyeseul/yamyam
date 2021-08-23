4cb1946bf07321229331615f8e8110a214423172
Yeseul Lee <sdc01194@gmail.com>
2021-08-23 22:45:05 +0900

    로그인 화면 작성

--- lib/src/view/screens/user/mypage/my_page_screen.dart
+++ lib/src/view/screens/user/mypage/my_page_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
+import 'package:yamstack/src/view/common/widgets/text/noto_sans_text.dart';
+import 'package:yamstack/src/view/screens/intro/splash/splash_screen.dart';
 import 'package:yamstack/src/view/screens/user/mypage/my_page_controller.dart';
 
 class MyPageScreen extends GetView<MyPageController> {
@@ -11,8 +13,17 @@ class MyPageScreen extends GetView<MyPageController> {
   @override
   Widget build(BuildContext context) => Scaffold(
         appBar: AppBar(),
-        body: const Center(
-          child: Text('MyPageScreen'),
+        body: InkWell(
+          onTap: () {
+            Get.offAllNamed(SplashScreen.route);
+          },
+          child: Center(
+            child: NotoSansText(
+              '처음으로',
+              size: 18,
+              underline: true,
+            ),
+          ),
         ),
       );
 }
--- lib/src/view/screens/user/signin/sign_in_controller.dart
+++ lib/src/view/screens/user/signin/sign_in_controller.dart
@@ -1,5 +1,27 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class SignInController extends GetxController {
+  final emailTextController = TextEditingController();
+  final passwordTextController = TextEditingController();
 
+  final _emailText = ''.obs;
+  final _passwordText = ''.obs;
+
+  String get emailText => _emailText.value;
+
+  String get passwordText => _passwordText.value;
+
+  void onEmailChanged(String email) {
+    _emailText(emailTextController.text);
+  }
+
+  void onPasswordChanged(String password) {
+    _passwordText(passwordTextController.text);
+  }
+
+  void onSignInButtonClicked() {}
+
+  bool get isAllFieldNotEmpty =>
+      _emailText.value.isNotEmpty && _passwordText.value.isNotEmpty;
 }
