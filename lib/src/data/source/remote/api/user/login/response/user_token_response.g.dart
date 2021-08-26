cd9116a9876a1c38e2a735913112ca6172475af8
Yeseul Lee <sdc01194@gmail.com>
2021-08-26 23:06:09 +0900

    삭제 파일 누락

--- lib/src/data/source/remote/api/user/login/response/user_token_response.g.dart
+++ lib/src/data/source/remote/api/user/login/response/user_token_response.g.dart
@@ -1,16 +1,16 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
-part of 'user_sign_response.dart';
+part of 'user_token_response.dart';
 
 // **************************************************************************
 // JsonSerializableGenerator
 // **************************************************************************
 
-UserSignResponse _$UserSignResponseFromJson(Map<String, dynamic> json) =>
-    UserSignResponse(
+UserTokenResponse _$UserTokenResponseFromJson(Map<String, dynamic> json) =>
+    UserTokenResponse(
       json['message'] as String? ?? '',
       json['code'] as String? ?? '',
-      json['status'] as int? ?? 0,
+      json['status'] == null ? 0 : BaseResponse.parseStatus(json['status']),
       json['accessToken'] as String?,
       json['refreshToken'] as String?,
     );
