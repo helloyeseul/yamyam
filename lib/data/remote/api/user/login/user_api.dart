ec93061e59e99ae92bf688f251b84ba3e3be5077
Yeseul Lee <sdc01194@gmail.com>
2021-08-30 22:52:18 +0900

    패키지 정리

--- lib/data/remote/api/user/login/user_api.dart
+++ lib/data/remote/api/user/login/user_api.dart
@@ -1,9 +1,12 @@
 import 'dart:async';
 
+import 'package:retrofit/http.dart';
 import 'package:yamstack/data/remote/api/user/login/request/user_join_request.dart';
 import 'package:yamstack/data/remote/response/base/base_single_response.dart';
 import 'package:yamstack/data/remote/response/base/empty_response.dart';
 
+@RestApi()
 abstract class UserApi {
+  @POST('/login/join')
   Future<BaseSingleResponse<EmptyResponse>> join(UserJoinRequest request);
 }
